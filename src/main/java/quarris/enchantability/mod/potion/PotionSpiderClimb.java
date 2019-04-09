fb22cce869068b3f23d6a398ca0c466ee86cd36f
Quarris <tomasz.winnicki@live.co.uk>
2019-04-09 17:59:31 +0100

    Slight problem with the Wall Climbing Ability, will keep it simplified for now.

--- src/main/java/quarris/enchantability/mod/potion/PotionSpiderClimb.java
+++ src/main/java/quarris/enchantability/mod/potion/PotionSpiderClimb.java
@@ -4,9 +4,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import quarris.enchantability.mod.Enchantability;
 
@@ -29,17 +32,34 @@ public class PotionSpiderClimb extends Potion {
 
 	@Override
 	public void performEffect(EntityLivingBase entity, int amplifier) {
-
+		World world = entity.world;
 		if (entity.collidedHorizontally) {
-			entity.motionY = 0.2 + amplifier*0.05;
+			entity.motionY = 0.15d + amplifier * 0.05d;
+			entity.fallDistance = 0;
 		}
-		else if (entity.isSneaking()) {
-			World world = entity.world;
+		else if (false) { // TODO: Allow sneaking to stop midair like on ladders
+			boolean blockNearby = false;
 			for (EnumFacing facing : EnumFacing.HORIZONTALS) {
-				IBlockState state = world.getBlockState(entity.getPosition().offset(facing));
+				BlockPos statePos = new BlockPos(entity.getPosition().getX()+facing.getFrontOffsetX(), entity.getPosition().getY(), entity.getPosition().getZ()+facing.getFrontOffsetZ());
+				IBlockState state = world.getBlockState(statePos);
 				if (state.getMaterial() != Material.AIR) {
-					entity.motionY = 0;
+					blockNearby = true;
 				}
+			}
+			if (blockNearby) {
+				entity.motionX = MathHelper.clamp(entity.motionX, -0.15d, 0.15d);
+				entity.motionZ = MathHelper.clamp(entity.motionZ, -0.15d, 0.15d);
+				entity.fallDistance = 0.0F;
+
+				if (entity.motionY < -0.15D) {
+					entity.motionY = -0.15D;
+				}
+
+				if (entity.isSneaking() && entity.motionY < 0.0D) {
+					System.out.println("Stop");
+					entity.motionY = 0.0D;
+				}
+				entity.fallDistance = 0;
 			}
 		}
 	}
