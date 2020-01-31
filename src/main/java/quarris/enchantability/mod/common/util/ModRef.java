c6df03fb60f41a82e6e987a7a736aaa26593f669
Quarris <tomasz.winnicki@live.co.uk>
2020-01-31 22:29:14 +0000

    no version

--- src/main/java/quarris/enchantability/mod/common/util/ModRef.java
+++ src/main/java/quarris/enchantability/mod/common/util/ModRef.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 public class ModRef {
     public static final String ID = "enchantability";
     public static final String NAME = "Enchantability";
-    public static final String VERSION = "1.3.0";
 
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final UUID ENCHANT_UUID = UUID.fromString("0DD5A1AD-CA11-ADD5-1CED-C0FFEEEFFEC7");
