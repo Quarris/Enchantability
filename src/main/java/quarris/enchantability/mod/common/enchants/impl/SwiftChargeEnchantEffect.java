281819196d1b4f0653661dc97f4af376212077b7
Quarris <tomasz.winnicki@live.co.uk>
2020-02-06 07:28:06 +0000

    Swift Charge pt2 Swinging

--- src/main/java/quarris/enchantability/mod/common/enchants/impl/SwiftChargeEnchantEffect.java
+++ src/main/java/quarris/enchantability/mod/common/enchants/impl/SwiftChargeEnchantEffect.java
@@ -1,9 +1,12 @@
 package quarris.enchantability.mod.common.enchants.impl;
 
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PotionItem;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import quarris.enchantability.api.enchants.AbstractEnchantEffect;
@@ -17,10 +20,29 @@ public class SwiftChargeEnchantEffect extends AbstractEnchantEffect {
         super(player, enchantment, level);
     }
 
+    @Override
+    public void onApplied() {
+        player.getAttribute(SharedMonsterAttributes.ATTACK_SPEED).applyModifier(new AttributeModifier(
+                ModRef.ENCHANT_UUID, NAME::toString, this.level/2f, AttributeModifier.Operation.ADDITION
+        ));
+    }
+
+    @Override
+    public void onRemoved() {
+        player.getAttribute(SharedMonsterAttributes.ATTACK_SPEED).removeModifier(ModRef.ENCHANT_UUID);
+    }
+
     public static void itemUse(SwiftChargeEnchantEffect effect, LivingEntityUseItemEvent.Start event) {
         ItemStack item = event.getItem();
         if (item.isFood() || item.getItem() instanceof PotionItem) {
-            event.setDuration(event.getDuration()/(effect.level+1));
+            event.setDuration(event.getDuration() / (effect.level + 1));
+        }
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        if (this.player.getAttribute(SharedMonsterAttributes.ATTACK_SPEED).getModifier(ModRef.ENCHANT_UUID) == null) {
+            this.onApplied();
         }
     }
 
